cfe53bb1be468aca06807e6efc5fee571e8c59f3
RJGJ <jaictinrj@gmail.com>
2022-08-24 08:28:53 +0800

    added remove from cart and alpinejs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,41 @@
+import Alpine from 'alpinejs'
+
 import './styles/index.styl'
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('hello world')
+window.Alpine = Alpine
+
+document.addEventListener('alpine:init', () => {
+  console.log('hello from alpine')  
 })
+
+Alpine.start()
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('hello world')
+
+
+
+//   // cart listeners
+//   document
+//     .querySelectorAll('[data-remove-line-item]')
+//     .forEach(remove => {
+//       const product_id = remove.getAttribute('data-remove-line-item')
+//       remove.addEventListener('click', () => {
+//         console.log(product_id)
+
+//         fetch('/cart/change.js', {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify({
+//             quantity: 0,
+//             id: product_id
+//           })
+//         })
+//           .then(response => response.json())
+//           .then(json => console.log(json))
+//           .catch(err => console.err(err))
+//       })
+//     })
+// })
